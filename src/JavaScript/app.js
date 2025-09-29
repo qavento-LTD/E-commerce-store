2918d7b8a766d873acc992f6049aa3e7e7532268
mohammed <qavento.ltd@gmail.com>
2025-09-29 03:19:27 +0300

    updateing 2

--- src/JavaScript/app.js
+++ src/JavaScript/app.js
@@ -6,8 +6,9 @@ nav.style.left = '100%';
 src_menu.src = 'public/icon/menu_open.svg';
 menu.onclick = () => {
     if (nav.style.left === '100%') {
-      nav.style.left = '0';
+      nav.style.left = '30%';
       src_menu.src = 'public/icon/menu_close.svg';
+      
       
     } else {
       nav.style.left = '100%';
